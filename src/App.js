86dbf71085446c697308c1a50f29d1e8a46e1c0e
Wellington Matheus <w.matheus@outlook.com>
2021-01-13 13:06:54 -0800

    feat: add styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import Select from 'components/select';
-
 function App() {
-  return (
-    <div className="App">
-      <Select name="Input de teste" label="Label" description="description" />
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
