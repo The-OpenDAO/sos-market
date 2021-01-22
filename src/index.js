cc523e37d1325b8aae118ac4283726c49fe32f2c
Wellington Matheus <w.matheus@outlook.com>
2021-01-22 11:39:35 -0800

    feat: add Gilroy font globally

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'styles/application.scss';
+import 'styles/app.scss';
 
 ReactDOM.render(
   <React.StrictMode>
