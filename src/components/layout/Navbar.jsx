147118843edc55abbad80262ba6c13ea82bd17ee
Wellington Matheus <w.matheus@outlook.com>
2021-01-22 17:55:09 -0800

    feat: create Navbar links

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,6 +5,7 @@ import styles from 'styles/layout/Navbar.module.scss';
 import { PolkamarketsLogo } from 'assets/icons';
 
 import { SearchBar } from 'components/base';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -13,6 +14,33 @@ function Navbar() {
         <PolkamarketsLogo />
       </div>
       <SearchBar />
+      <div className={styles.header}>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink className={styles.item} exact to="/markets">
+              Markets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={styles.item} to="/portfolio">
+              Porfolio
+            </NavLink>
+          </li>
+          <div className={styles.separator} />
+          <ul className={styles.menu}>
+            <li>
+              <NavLink className={styles.item} exact to="/howitworks">
+                How It Works
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={styles.item} to="/faq">
+                FAQ
+              </NavLink>
+            </li>
+          </ul>
+        </ul>
+      </div>
     </div>
   );
 }
