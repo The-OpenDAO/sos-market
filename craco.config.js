3140eb40bab135c65b30863bdfb020c99a65f812
Wellington Matheus <w.matheus@outlook.com>
2021-02-19 12:56:03 -0300

    feat: create abstract folder in sass schema

--- craco.config.js
+++ craco.config.js
@@ -6,8 +6,8 @@ module.exports = {
       plugin: sassResourcesLoader,
       options: {
         resources: [
-          './src/styles/base/_colors.scss',
-          './src/styles/utils/_mixins.scss'
+          './src/styles/abstracts/_colors.scss',
+          './src/styles/abstracts/_mixins.scss'
         ]
       }
     }
