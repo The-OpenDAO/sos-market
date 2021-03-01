fe9ba3589800a22cc0021be7e3c9f3d178cbd151
WellingtonMatheus <w.matheus@outlook.com>
2021-03-01 08:46:34 -0300

    hotfix: hide components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import React from 'react';
 
-import Routes from 'routes';
-
-import { Layout } from 'components';
-
 function App() {
-  const [dark, setDark] = React.useState(false);
-
-  return (
-    <div className={`${dark ? 'theme--dark' : 'theme--light'}`}>
-      <button type="button" onClick={() => setDark(!dark)}>
-        Change Theme
-      </button>
-      <Layout>
-        <Routes />
-      </Layout>
-    </div>
-  );
+  return <div className="app" />;
 }
 
 export default App;
