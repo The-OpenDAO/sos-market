e7c9ee097c7c0c680b6fa602981d840cf6a98278
WellingtonMatheus <w.matheus@outlook.com>
2021-03-01 16:23:54 -0300

    feat: detect browser theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Routes from 'routes';
 
@@ -6,6 +6,15 @@ import { Layout } from 'components';
 
 function App() {
   const [dark, setDark] = React.useState(false);
+  const browserUseDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+  useEffect(() => {
+    if (browserUseDarkTheme.matches) {
+      setDark(true);
+    } else {
+      setDark(false);
+    }
+  }, [browserUseDarkTheme.matches]);
 
   return (
     <div className={`${dark ? 'theme--dark' : 'theme--light'}`}>
