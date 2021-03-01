f18cf297e9e3d7d1464d21642733badf337ed10e
WellingtonMatheus <w.matheus@outlook.com>
2021-03-01 08:47:06 -0300

    Revert "hotfix: hide components"
    This reverts commit fe9ba3589800a22cc0021be7e3c9f3d178cbd151.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Routes from 'routes';
+
+import { Layout } from 'components';
+
 function App() {
-  return <div className="app" />;
+  const [dark, setDark] = React.useState(false);
+
+  return (
+    <div className={`${dark ? 'theme--dark' : 'theme--light'}`}>
+      <button type="button" onClick={() => setDark(!dark)}>
+        Change Theme
+      </button>
+      <Layout>
+        <Routes />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
