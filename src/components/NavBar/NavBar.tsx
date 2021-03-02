bf582490e066b36c248f5694d8f69a7c5ab32069
WellingtonMatheus <w.matheus@outlook.com>
2021-03-01 23:50:32 -0300

    refactor: add new theme variables to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { PolkamarketsIcon } from 'assets/icons';
 import { SearchBar } from 'components';
@@ -10,6 +11,16 @@ const NavBar = () => {
         <PolkamarketsIcon />
       </figure>
       <SearchBar />
+      <ul className="navbar-menu">
+        <li className="navbar-menu__item">
+          <NavLink exact to="/howitworks">
+            How It Works
+          </NavLink>
+        </li>
+        <li className="navbar-menu__item">
+          <NavLink to="/faq">FAQ</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
