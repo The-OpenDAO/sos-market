ac6622d6d19367bf9f20f03a51b95a19c88ceef6
Wellington Matheus <w.matheus@outlook.com>
2021-03-12 01:27:06 -0300

    fix: decrease MiniTable gap

--- src/components/QuickTrade/mock.ts
+++ src/components/QuickTrade/mock.ts
@@ -1,6 +1,6 @@
 const tableItems = [
-  { name: 'Predictions', value: 'ADA' },
-  { name: 'Price Per Fraction', value: '0.000034 DOT' },
+  { name: 'Prediction', value: 'ADA' },
+  { name: 'Price Per Fraction', value: '0.00034 DOT' },
   { name: 'Fractions Bought', value: '3,5' },
   { name: 'Current ROI', value: '2,5%' },
   { name: 'Total Stake', value: '0.245 DOT' },
