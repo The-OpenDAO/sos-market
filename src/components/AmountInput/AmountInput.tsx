caaf7c863979f5f21dded55bdd8402fff826265d
Wellington Matheus <w.matheus@outlook.com>
2021-03-15 13:24:02 -0300

    feat: add AmountInput actions

--- src/components/AmountInput/AmountInput.tsx
+++ src/components/AmountInput/AmountInput.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 
+import { PolkadotIcon } from 'assets/icons';
+
+import Text from '../Text';
+import Button from '../Button';
+
 interface Props {
   label: string;
   max: number;
@@ -19,17 +24,35 @@ const AmountInput = ({ label, max }: Props) => {
       <label className="amount-input__label" htmlFor={label}>
         {label}
       </label>
-      <input
-        className="amount-input__input"
-        type="number"
-        id={label}
-        value={amount}
-        step=".0001"
-        min={0}
-        max={max}
-        onChange={event => handleChangeAmount(event)}
-        onError={event => console.error(event)}
-      />
+      <div className="amount-input__group">
+        <input
+          className="amount-input__input"
+          type="number"
+          id={label}
+          value={amount}
+          step=".0001"
+          min={0}
+          max={max}
+          onChange={event => handleChangeAmount(event)}
+          onError={event => console.error(event)}
+        />
+        <div className="amount-input__actions">
+          <button type="button">
+            <Text as="span" scale="tiny-uppercase" fontWeight="semibold">
+              Max
+            </Text>
+          </button>
+          <Button
+            variant="noborder"
+            icon={<PolkadotIcon />}
+            iconPosition="left"
+          >
+            <Text as="span" scale="caption" fontWeight="bold">
+              DOT
+            </Text>
+          </Button>
+        </div>
+      </div>
     </form>
   );
 };
