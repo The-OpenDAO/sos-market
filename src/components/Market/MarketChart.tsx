1b6a04cd91f59e3fbdeadaa814743ed637d8ecdc
Wellington Matheus <w.matheus@outlook.com>
2021-03-17 22:51:25 -0300

    fix(MarketChart): reduce column width

--- src/components/Market/MarketChart.tsx
+++ src/components/Market/MarketChart.tsx
@@ -12,67 +12,72 @@ type MarketChartProps = {
   serie: Event[];
 };
 
+const chartOptions = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    type: 'candlestick',
+    height: 288
+  },
+  xaxis: {
+    type: 'datetime',
+    tooltip: {
+      enabled: false
+    },
+    axisTicks: {
+      show: false
+    },
+    axisBorder: {
+      show: false
+    }
+  },
+  yaxis: {
+    show: false,
+    tooltip: {
+      enabled: true
+    }
+  },
+  grid: {
+    show: true,
+    borderColor: '#252C3B',
+    strokeDashArray: 5,
+    position: 'back',
+    xaxis: {
+      lines: {
+        show: true
+      }
+    },
+    yaxis: {
+      lines: {
+        show: true
+      }
+    }
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '25%'
+    },
+    candlestick: {
+      colors: {
+        upward: '#46D39A',
+        downward: '#EF4444'
+      },
+      wick: {
+        useFillColor: true
+      }
+    }
+  }
+};
+
 const MarketChart = ({ serie }: MarketChartProps) => {
   return (
     <div className="market-chart__chart">
       <ReactApexChart
-        options={{
-          chart: {
-            toolbar: {
-              show: false
-            },
-            type: 'candlestick',
-            height: 288
-          },
-          xaxis: {
-            type: 'datetime',
-            tooltip: {
-              enabled: false
-            },
-            axisTicks: {
-              show: false
-            },
-            axisBorder: {
-              show: false
-            }
-          },
-          yaxis: {
-            show: false,
-            tooltip: {
-              enabled: true
-            }
-          },
-          grid: {
-            show: true,
-            borderColor: '#252C3B',
-            strokeDashArray: 5,
-            position: 'back',
-            xaxis: {
-              lines: {
-                show: true
-              }
-            },
-            yaxis: {
-              lines: {
-                show: true
-              }
-            }
-          },
-          plotOptions: {
-            candlestick: {
-              colors: {
-                upward: '#46D39A',
-                downward: '#EF4444'
-              },
-              wick: {
-                useFillColor: true
-              }
-            }
-          }
-        }}
+        options={chartOptions}
         series={[{ data: serie }]}
         type="candlestick"
-        height={350}
+        height={288}
       />
     </div>
   );
