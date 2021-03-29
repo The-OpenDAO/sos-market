b757435ea97f3f93e33e7707cb8288e999a1a618
Wellington Matheus <w.matheus@outlook.com>
2021-03-28 23:06:34 -0300

    fix(Table): add table body tag

--- src/components/MarketTable/MarketTable.tsx
+++ src/components/MarketTable/MarketTable.tsx
@@ -41,72 +41,74 @@ type MarketTableProps = {
 const MarketTable = ({ rows }: MarketTableProps) => {
   return (
     <table className="market-table">
-      <tr className="market-table__header">
-        {headers?.map(header => (
-          <th className="market-table__header-item" key={header}>
-            {header}
-          </th>
-        ))}
-      </tr>
-      {rows?.map(row => (
-        <tr className="market-table__row" key={row.id}>
-          <td id="market" className="market-table__row-item">
-            <img
-              className="market-table__row-item__image"
-              src={row.imageUrl}
-              alt={row.id}
-            />
-            {row.description}
-          </td>
-          <td id="price" className="market-table__row-item">
-            <div className="market-table__row-item__group">
-              {row.price.value}
-              <Text
-                className={`market-table__row-item__change--${row.price.change.type}`}
-                as="span"
-                scale="caption"
-                fontWeight="bold"
-              >
-                {row.price.change.type === 'up' ? (
-                  <CaretUpIcon />
-                ) : (
-                  <CaretDownIcon />
-                )}
-                {`${row.price.change.value}%`}
-              </Text>
-            </div>
-          </td>
-          <td id="profit" className="market-table__row-item">
-            <div className="market-table__row-item__group">
-              {row.profit.value}
-              <Text
-                className={`market-table__row-item__change--${row.profit.change.type}`}
-                as="span"
-                scale="caption"
-                fontWeight="bold"
-              >
-                {row.profit.change.type === 'up' ? (
-                  <CaretUpIcon />
-                ) : (
-                  <CaretDownIcon />
-                )}
-                {`${row.profit.change.value}%`}
-              </Text>
-            </div>
-          </td>
-          <td id="fractions" className="market-table__row-item">
-            {row.fractions}
-          </td>
-          <td id="maxPayout" className="market-table__row-item">
-            {row.maxPayout}
-          </td>
-          <td id="trade" className="market-table__row-item">
-            <Button color="primary" size="sm">
-              Trade
-            </Button>
-          </td>
+      <tbody>
+        <tr className="market-table__header">
+          {headers?.map(header => (
+            <th className="market-table__header-item" key={header}>
+              {header}
+            </th>
+          ))}
         </tr>
-      ))}
+        {rows?.map(row => (
+          <tr className="market-table__row" key={row.id}>
+            <td id="market" className="market-table__row-item">
+              <img
+                className="market-table__row-item__image"
+                src={row.imageUrl}
+                alt={row.id}
+              />
+              {row.description}
+            </td>
+            <td id="price" className="market-table__row-item">
+              <div className="market-table__row-item__group">
+                {row.price.value}
+                <Text
+                  className={`market-table__row-item__change--${row.price.change.type}`}
+                  as="span"
+                  scale="caption"
+                  fontWeight="bold"
+                >
+                  {row.price.change.type === 'up' ? (
+                    <CaretUpIcon />
+                  ) : (
+                    <CaretDownIcon />
+                  )}
+                  {`${row.price.change.value}%`}
+                </Text>
+              </div>
+            </td>
+            <td id="profit" className="market-table__row-item">
+              <div className="market-table__row-item__group">
+                {row.profit.value}
+                <Text
+                  className={`market-table__row-item__change--${row.profit.change.type}`}
+                  as="span"
+                  scale="caption"
+                  fontWeight="bold"
+                >
+                  {row.profit.change.type === 'up' ? (
+                    <CaretUpIcon />
+                  ) : (
+                    <CaretDownIcon />
+                  )}
+                  {`${row.profit.change.value}%`}
+                </Text>
+              </div>
+            </td>
+            <td id="fractions" className="market-table__row-item">
+              {row.fractions}
+            </td>
+            <td id="maxPayout" className="market-table__row-item">
+              {row.maxPayout}
+            </td>
+            <td id="trade" className="market-table__row-item">
+              <Button color="primary" size="sm">
+                Trade
+              </Button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
