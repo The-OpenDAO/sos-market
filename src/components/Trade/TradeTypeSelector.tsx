a78dd33d459731ef02843e162be6572346eb7847
Wellington Matheus <w.matheus@outlook.com>
2021-04-02 02:34:53 -0300

    feat(Trade): create TradeTypeSelector

---
+++ src/components/Trade/TradeTypeSelector.tsx
@@ -0,0 +1,33 @@
+import isUndefined from 'lodash/isUndefined';
+import { changeTradeType } from 'redux/ducks/trade';
+
+import { useAppDispatch } from 'hooks';
+
+import ToggleButton from '../ToggleButton';
+
+function TradeTypeSelector() {
+  const dispatch = useAppDispatch();
+
+  function handleChangeTradeType(tradeType: string | undefined) {
+    if (!isUndefined(tradeType)) {
+      dispatch(changeTradeType(tradeType));
+    }
+  }
+
+  return (
+    <div className="trade-type-selector">
+      <ToggleButton
+        defaultActiveId="buy"
+        buttons={[
+          { id: 'buy', name: 'Buy', variant: 'success' },
+          { id: 'sell', name: 'Sell', variant: 'danger' }
+        ]}
+        onChange={tradeType => handleChangeTradeType(tradeType)}
+      />
+    </div>
+  );
+}
+
+TradeTypeSelector.displayName = 'TradeTypeSelector';
+
+export default TradeTypeSelector;
