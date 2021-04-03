84d0681641b601c8f4149e819d55ddd44acb96ff
Wellington Matheus <w.matheus@outlook.com>
2021-04-03 18:32:38 -0300

    refactor(Market): use ChartHeader

--- src/pages/Market/MarketChart.tsx
+++ src/pages/Market/MarketChart.tsx
@@ -1,51 +1,19 @@
 import React, { useState, useMemo } from 'react';
 
-import clx from 'classnames';
-
 import { CandleStickChartIcon, LineChartIcon } from 'assets/icons';
 
-import { CandleStickChart, Text } from 'components';
+import { CandleStickChart, ChartHeader, Text } from 'components';
 
 import { generateMarketChartRandomData } from './utils';
 
-const intervalsInMinutes = {
-  '1H': 60,
-  '12H': 120,
-  '1D': 120,
-  '1M': 120
-};
-
-const views = [
-  {
-    name: 'candlestick',
-    icon: <CandleStickChartIcon />
-  },
-  {
-    name: 'line',
-    icon: <LineChartIcon />
-  }
-];
-
 const MarketChart = () => {
-  const [interval, setInterval] = useState('1H');
-  const [view, setView] = useState('candlestick');
+  const [currentInterval, setCurrentInterval] = useState(60);
+  const [currentView, setCurrentView] = useState('candleStick');
 
   const randomMarketChartData = useMemo(
-    () => generateMarketChartRandomData(intervalsInMinutes[interval]),
-    [interval]
+    () => generateMarketChartRandomData(currentInterval),
+    [currentInterval]
   );
-
-  function handleChangeInterval(event: React.MouseEvent<HTMLButtonElement>) {
-    const { value } = event.currentTarget;
-
-    setInterval(value);
-  }
-
-  function handleChangeView(event: React.MouseEvent<HTMLButtonElement>) {
-    const { value } = event.currentTarget;
-
-    setView(value);
-  }
 
   return (
     <div className="market-chart">
@@ -59,42 +27,35 @@ const MarketChart = () => {
           Market Overview
         </Text>
         <div className="market-chart__header-actions">
-          <div className="market-chart__header-actions__interval">
-            {Object.keys(intervalsInMinutes).map(key => (
-              <button
-                key={key}
-                className={clx({
-                  'market-chart__header-actions__interval-item': true,
-                  active: key === interval
-                })}
-                type="button"
-                value={key}
-                onClick={event => handleChangeInterval(event)}
-              >
-                {key}
-              </button>
-            ))}
-          </div>
-          <div className="market-chart__header-actions__view">
-            {views.map(key => (
-              <button
-                key={key.name}
-                className={clx({
-                  'market-chart__header-actions__view-item': true,
-                  active: key.name === view
-                })}
-                type="button"
-                value={key.name}
-                onClick={event => handleChangeView(event)}
-              >
-                {key.icon}
-              </button>
-            ))}
-          </div>
+          <ChartHeader
+            intervals={[
+              { id: 'hour', name: '1H', value: 60 },
+              { id: '12hour', name: '12H', value: 120 },
+              { id: 'day', name: '1D', value: 120 },
+              { id: 'month', name: '1M', value: 120 }
+            ]}
+            views={[
+              {
+                id: 'candleStick',
+                icon: <CandleStickChartIcon />
+              },
+              {
+                id: 'line',
+                icon: <LineChartIcon />
+              }
+            ]}
+            defaultIntervalId="hour"
+            defaultViewId="candleStick"
+            onChangeInterval={(interval, value) => setCurrentInterval(value)}
+            onChangeView={view => setCurrentView(view)}
+          />
         </div>
       </div>
+
       <div className="market-chart__view">
-        <CandleStickChart serie={randomMarketChartData} height={288} />
+        {currentView === 'candleStick' ? (
+          <CandleStickChart serie={randomMarketChartData} height={288} />
+        ) : null}
       </div>
     </div>
   );
