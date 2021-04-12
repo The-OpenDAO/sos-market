86efea68fe6e16affd4e349fcb88af0150aa6cd1
Wellington Matheus <w.matheus@outlook.com>
2021-04-11 21:06:35 -0300

    feat(Redux): create markets slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,17 @@
+import { Dispatch } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { configureStore } from '@reduxjs/toolkit';
 import thunkMiddleware from 'redux-thunk';
 
 import bepro from './ducks/bepro';
+import markets from './ducks/markets';
 import trade from './ducks/trade';
 
 const store = configureStore({
   reducer: {
     trade,
+    markets,
     bepro
   },
   middleware: [thunkMiddleware],
@@ -18,5 +21,5 @@ const store = configureStore({
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch | Dispatch<any>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
