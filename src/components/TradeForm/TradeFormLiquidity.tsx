14d0216dee9cb440f8ac2069ad796467e8cc5c7d
Wellington Matheus <w.matheus@outlook.com>
2021-04-13 02:24:38 -0300

    feat(TradeForm): integrate with LiquidityForm

--- src/components/TradeForm/TradeFormLiquidity.tsx
+++ src/components/TradeForm/TradeFormLiquidity.tsx
@@ -1,11 +1,21 @@
+import { openForm } from 'redux/ducks/liquidity';
+
 import { InfoIcon } from 'assets/icons';
+
+import { useAppDispatch } from 'hooks';
 
 import { Button } from '../Button';
 
 function TradeFormLiquidity() {
+  const dispatch = useAppDispatch();
+
+  function handleAddLiquidity() {
+    dispatch(openForm());
+  }
+
   return (
     <div className="pm-c-trade-form-liquidity">
-      <Button variant="dark" size="sm" fullWidth>
+      <Button variant="dark" size="sm" fullWidth onClick={handleAddLiquidity}>
         <span>Add Liquidity</span>
         <InfoIcon />
       </Button>
