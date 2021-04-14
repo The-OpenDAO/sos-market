c44c7bd6564d8b855fa1df007a214b31f3a35872
Wellington Matheus <w.matheus@outlook.com>
2021-04-13 21:30:26 -0300

    feat(RightSidebar): check own visibility

--- src/components/RightSidebar/RightSidebar.tsx
+++ src/components/RightSidebar/RightSidebar.tsx
@@ -4,12 +4,17 @@ import LiquidityForm from '../LiquidityForm';
 import TradeForm from '../TradeForm';
 
 function RightSidebar() {
+  const rightSidebarIsVisible = useAppSelector(
+    state => state.ui.rightSidebar.visible
+  );
   const tradeFormIsVisible = useAppSelector(
     state => state.ui.tradeForm.visible
   );
   const liquidityFormIsVisible = useAppSelector(
     state => state.ui.liquidityForm.visible
   );
+
+  if (!rightSidebarIsVisible) return null;
 
   if (tradeFormIsVisible)
     return (
