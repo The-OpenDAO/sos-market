a61943da2b239eb327e08c408ced210470426fbd
Wellington Matheus <w.matheus@outlook.com>
2021-04-14 17:21:24 -0300

    feat(TradeForm): auto select outcome if has any selected market

--- src/components/TradeForm/TradeForm.tsx
+++ src/components/TradeForm/TradeForm.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { changePredictionsVisibility, selectOutcome } from 'redux/ducks/trade';
+import { openTradeForm } from 'redux/ducks/ui';
+
+import { useAppDispatch, useAppSelector } from 'hooks';
+
 import TradeFormActions from './TradeFormActions';
 import TradeFormCharts from './TradeFormCharts';
 import TradeFormDetails from './TradeFormDetails';
@@ -8,6 +16,19 @@ import TradeFormTerms from './TradeFormTerms';
 import TradeFormTypeSelector from './TradeFormTypeSelector';
 
 function TradeForm() {
+  const dispatch = useAppDispatch();
+  const { id, outcomes } = useAppSelector(state => state.market.market);
+  const selectedMarketId = useAppSelector(
+    state => state.trade.selectedMarketId
+  );
+
+  useEffect(() => {
+    if (id !== '' && id !== selectedMarketId) {
+      dispatch(changePredictionsVisibility(true));
+      dispatch(selectOutcome(id, outcomes[0].id));
+    }
+  }, [dispatch, id, outcomes, selectedMarketId]);
+
   return (
     <div className="pm-c-trade-form">
       <div className="pm-c-trade-form__group" style={{ gap: '1.6rem' }}>
