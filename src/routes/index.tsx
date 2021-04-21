d4440ed15a09d9f394f0ffe4ddab32b3296171d7
Wellington Matheus <w.matheus@outlook.com>
2021-04-21 14:34:26 -0300

    feat(Routes): check if wallet is on localStorage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
-import { useAppSelector } from 'hooks';
+import { isUndefined } from 'lodash';
+
+import { useAppSelector, useLocalStorage } from 'hooks';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
 export default function Routes() {
   const walletConnected = useAppSelector(state => state.bepro.isLoggedIn);
+  const [walletAddress] = useLocalStorage('walletAddress', undefined);
 
-  return walletConnected ? <AppRoutes /> : <AuthRoutes />;
+  return walletConnected || !isUndefined(walletAddress) ? (
+    <AppRoutes />
+  ) : (
+    <AuthRoutes />
+  );
 }
