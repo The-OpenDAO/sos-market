1d604198b8e6941c06d5c2de6dfbd4c588ce0f96
Wellington Matheus <w.matheus@outlook.com>
2021-04-21 14:33:59 -0300

    refactor(PrivateBeta): create wallet address form

--- src/pages/PrivateBeta/PrivateBeta.tsx
+++ src/pages/PrivateBeta/PrivateBeta.tsx
@@ -1,15 +1,32 @@
+import React, { useState } from 'react';
+
 import { fetchWallet } from 'redux/ducks/bepro';
 import { BeproService } from 'services';
 
-import { PolkamarketsIcon } from 'assets/icons';
+import { ArrowRightIcon, PolkamarketsIcon } from 'assets/icons';
 
 import { Button, ModalNotification, Text } from 'components';
 
-import { useAppDispatch } from 'hooks';
+import { useAppDispatch, useLocalStorage } from 'hooks';
 
 function PrivateBeta() {
+  const [walletAddress, setWalletAddress] = useLocalStorage(
+    'walletAddress',
+    undefined
+  );
+  const [address, setAddress] = useState('');
   const dispatch = useAppDispatch();
   const beproService = new BeproService();
+
+  function handleChangeAddress(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    setAddress(value);
+  }
+
+  function handleSubmit() {
+    setWalletAddress(address);
+  }
 
   const handleConnectWallet = async () => {
     await beproService.login();
@@ -41,9 +58,19 @@ function PrivateBeta() {
             use our platform. Connect your wallet and start predicting a better
             future.
           </Text>
-          <Button color="primary" fullWidth onClick={handleConnectWallet}>
-            Connect with Metamask
-          </Button>
+          <form className="pm-private-beta__form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="pm-private-beta__form-input"
+              name="address"
+              placeholder="Enter your wallet address"
+              value={address}
+              onChange={event => handleChangeAddress(event)}
+            />
+            <Button type="submit" color="primary" size="lg">
+              <ArrowRightIcon />
+            </Button>
+          </form>
         </div>
       </ModalNotification>
     </div>
