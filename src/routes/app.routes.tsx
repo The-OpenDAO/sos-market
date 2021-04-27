399183ab7fc4cfbed5bcba9a66512cf87ae2953e
Wellington Matheus <w.matheus@outlook.com>
2021-04-26 23:02:10 -0300

    feat(AppRoutes): check connected network

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,13 +1,25 @@
 import { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import WrongNetwork from 'pages/WrongNetwork';
+
 import { Layout } from 'components';
+
+import { useNetwork } from 'hooks';
 
 const Home = lazy(() => import('pages/Home'));
 const Market = lazy(() => import('pages/Market'));
 const Portfolio = lazy(() => import('pages/Portfolio'));
 
+const { REACT_APP_NETWORK_ID } = process.env;
+
 const AppRoutes = () => {
+  const { network } = useNetwork();
+
+  const isAllowedNetwork = network?.id === REACT_APP_NETWORK_ID;
+
+  if (!isAllowedNetwork) return <WrongNetwork />;
+
   return (
     <Layout>
       <Suspense fallback={null}>
