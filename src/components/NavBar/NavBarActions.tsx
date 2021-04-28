ca8cd13dcf3e337e2a8d224c6916768ce30339ab
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-04-28 12:12:22 +0800

    feat(NavBar): link to google form on create market

--- src/components/NavBar/NavBarActions.tsx
+++ src/components/NavBar/NavBarActions.tsx
@@ -69,7 +69,7 @@ function NavBarActions() {
           className="button-normal--default pm-c-navbar__actions-create-market"
           aria-label="Create market"
           target="_blank"
-          href="https://www.google.com/forms/about"
+          href="https://docs.google.com/forms/d/1WA_WQ3Ma6iXr1HExs541cmquoI4n3SysI_DGuDyrRVQ/"
           rel="noreferrer"
         >
           <AddIcon />
