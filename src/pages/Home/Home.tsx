94ae186d6329bec8a663701c966a7d2ab7ceeb64
Wellington Matheus <w.matheus@outlook.com>
2021-06-04 11:10:56 -0300

    feat(Home): create HomeTabs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,12 @@ import { useEffect } from 'react';
 
 import { filteredMarketsSelector, getMarkets } from 'redux/ducks/markets';
 
-import { Tabs, MarketList } from 'components';
-
 import { useAppDispatch, useAppSelector } from 'hooks';
 import useCategories from 'hooks/useCategories';
 
 import HomeCategories from './HomeCategories';
 import HomeMobileInfo from './HomeMobileInfo';
+import HomeTabs from './HomeTabs';
 
 function Home() {
   const dispatch = useAppDispatch();
@@ -30,26 +29,11 @@ function Home() {
       <div className="pm-home__content">
         <HomeMobileInfo />
         <HomeCategories />
-
-        {/* <div className="navigation">
-          <div className="filters">
-            <Select label="Sort by:" name="Sort by" disabled>
-              <option value="Most traded">Most traded</option>
-            </Select>
-          </div>
-        </div> */}
-
-        <Tabs defaultActiveId="open">
-          <Tabs.TabPane tab="Open" id="open">
-            <MarketList markets={openMarkets} />
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="In-Reporting" id="in-reporting">
-            <MarketList markets={closedMarkets} />
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Resolved" id="resolved">
-            <MarketList markets={resolvedMarkets} />
-          </Tabs.TabPane>
-        </Tabs>
+        <HomeTabs
+          openMarkets={openMarkets}
+          closedMarkets={closedMarkets}
+          resolvedMarkets={resolvedMarkets}
+        />
       </div>
     </div>
   );
