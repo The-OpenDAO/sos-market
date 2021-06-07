b17aaca344196dfc78a7140f22a39a2c99b83aa6
Wellington Matheus <w.matheus@outlook.com>
2021-06-07 14:05:58 -0300

    refactor(PortfolioPage): move analytics to new PortfolioAnalytics component

--- src/pages/Portfolio/PortfolioPage.tsx
+++ src/pages/Portfolio/PortfolioPage.tsx
@@ -4,25 +4,20 @@ import { getMarkets } from 'redux/ducks/markets';
 import { getPortfolio } from 'redux/ducks/portfolio';
 import { closeRightSidebar } from 'redux/ducks/ui';
 
-import { CategoryAnalytics } from 'components';
-
 import { useAppDispatch, useAppSelector } from 'hooks';
-import useCurrency from 'hooks/useCurrency';
 
+import PortfolioAnalytics from './PortfolioAnalytics';
 import PortfolioChart from './PortfolioChart';
 import PortfolioTabs from './PortfolioTabs';
-import { formatPortfolioAnalytics } from './utils';
 
 const PortfolioPage = () => {
   const dispatch = useAppDispatch();
-  const { ticker } = useCurrency();
+
   const rightSidebarIsVisible = useAppSelector(
     state => state.ui.rightSidebar.visible
   );
 
   const ethAddress = useAppSelector(state => state.bepro.ethAddress);
-  // portfolio stats fetched from api
-  const apiPortfolio = useAppSelector(state => state.portfolio.portfolio);
 
   useEffect(() => {
     if (rightSidebarIsVisible) {
@@ -37,25 +32,9 @@ const PortfolioPage = () => {
     }
   }, [ethAddress, dispatch]);
 
-  const analytics = formatPortfolioAnalytics(apiPortfolio, ticker);
-
   return (
     <div className="portfolio-page">
-      <ul className="portfolio-page__analytics">
-        {analytics?.map(
-          ({ title, value, change, chartData, backgroundColor }) => (
-            <li key={title}>
-              <CategoryAnalytics
-                title={title}
-                value={value}
-                change={change}
-                chartData={chartData}
-                backgroundColor={backgroundColor}
-              />
-            </li>
-          )
-        )}
-      </ul>
+      <PortfolioAnalytics />
       <PortfolioChart />
       <PortfolioTabs />
     </div>
