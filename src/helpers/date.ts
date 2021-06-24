9637f7abb0a043308c16c1135e2efbbdce9701c6
Wellington Matheus <w.matheus@outlook.com>
2021-06-24 18:37:27 -0300

    feat(relativeTimeFromNow): customize copy text

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 import utc from 'dayjs/plugin/utc';
 
 function fromTimestampToDate(timestamp: number) {
@@ -9,6 +10,26 @@ function fromTimestampToDate(timestamp: number) {
 
 function relativeTimeFromNow(timestamp: number) {
   dayjs.extend(relativeTime);
+  dayjs.extend(updateLocale);
+
+  dayjs.updateLocale('en', {
+    relativeTime: {
+      future: '%s',
+      past: '%s ago',
+      s: 'a few seconds',
+      m: 'a minute',
+      mm: '%d minutes',
+      h: 'an hour',
+      hh: '%d hours',
+      d: 'a day',
+      dd: '%d days',
+      M: 'a month',
+      MM: '%d months',
+      y: 'a year',
+      yy: '%d years'
+    }
+  });
+
   const date = fromTimestampToDate(timestamp);
 
   return dayjs(date).fromNow();
