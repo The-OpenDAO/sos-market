55ce84b0b39535459e878b0b4b51d382280a2392
Wellington Matheus <w.matheus@outlook.com>
2021-06-24 17:56:25 -0300

    feat(ReportForm): create ReportFormActions

---
+++ src/components/ReportForm/ReportFormActions.tsx
@@ -0,0 +1,33 @@
+import { useField, useFormikContext } from 'formik';
+
+import { Button } from '../Button';
+
+function ReportFormActions() {
+  const [outcome] = useField('outcome');
+  const [bond] = useField('bond');
+  const { isSubmitting } = useFormikContext();
+
+  function handleCancel() {}
+
+  console.log(isSubmitting);
+
+  return (
+    <div className="pm-c-report-form-details__actions">
+      <Button variant="dark" color="default" size="lg" onClick={handleCancel}>
+        Cancel
+      </Button>
+      <Button
+        type="submit"
+        size="lg"
+        color="primary"
+        fullWidth
+        disabled={!outcome.value || !bond.value || bond.value === 0}
+        loading={isSubmitting}
+      >
+        Bond
+      </Button>
+    </div>
+  );
+}
+
+export default ReportFormActions;
