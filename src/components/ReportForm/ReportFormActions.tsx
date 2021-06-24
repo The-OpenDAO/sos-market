7df556cc3f78d49655e73499215bd0cd4b7f9647
Wellington Matheus <w.matheus@outlook.com>
2021-06-24 19:42:42 -0300

    feat(ReportFormActions): create handleCancel action

--- src/components/ReportForm/ReportFormActions.tsx
+++ src/components/ReportForm/ReportFormActions.tsx
@@ -1,13 +1,20 @@
 import { useField, useFormikContext } from 'formik';
+import { closeReportForm } from 'redux/ducks/ui';
+
+import { useAppDispatch } from 'hooks';
 
 import { Button } from '../Button';
 
 function ReportFormActions() {
+  const dispatch = useAppDispatch();
+
   const [outcome] = useField('outcome');
   const [bond] = useField('bond');
   const { isSubmitting } = useFormikContext();
 
-  function handleCancel() {}
+  function handleCancel() {
+    dispatch(closeReportForm());
+  }
 
   return (
     <div className="pm-c-report-form-details__actions">
