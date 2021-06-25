c0a5bfa8cbd7dcdedc94788bbb5f973026fd3370
Wellington Matheus <w.matheus@outlook.com>
2021-06-25 16:30:12 -0300

    feat(CreateMarketForm): add Formik

--- src/components/CreateMarketForm/CreateMarketForm.tsx
+++ src/components/CreateMarketForm/CreateMarketForm.tsx
@@ -1,10 +1,33 @@
+import { Formik, Form } from 'formik';
+
 import CreateMarketFormConfigure from './CreateMarketFormConfigure';
 
+type CreateMarketFormData = {
+  question: string;
+};
+
 function CreateMarketForm() {
+  const initialData: CreateMarketFormData = {
+    question: ''
+  };
+
+  async function handleFormSubmit(values: CreateMarketFormData) {
+    console.log(values);
+  }
+
   return (
-    <div className="pm-c-create-market-form">
-      <CreateMarketFormConfigure />
-    </div>
+    <Formik
+      initialValues={initialData}
+      onSubmit={async (values, actions) => {
+        actions.setSubmitting(true);
+        await handleFormSubmit(values);
+        actions.setSubmitting(false);
+      }}
+    >
+      <Form className="pm-c-create-market-form">
+        <CreateMarketFormConfigure />
+      </Form>
+    </Formik>
   );
 }
 
