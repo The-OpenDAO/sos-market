75d115ce08694e71684e819399cfe33500aace64
Wellington Matheus <w.matheus@outlook.com>
2021-06-25 12:36:00 -0300

    feat(ReportFormInfo): add Learn more link

--- src/components/ReportForm/ReportFormInfo.tsx
+++ src/components/ReportForm/ReportFormInfo.tsx
@@ -1,10 +1,22 @@
 import Alert from '../Alert';
+import Link from '../Link';
 
 function ReportFormInfo() {
   return (
     <Alert
       variant="information"
-      description="Earn $POLK by reporting the correct outcome."
+      description={
+        <>
+          {`Earn $POLK by reporting the correct outcome. `}
+          <Link
+            title="Learn more"
+            href="//www.polkamarkets.com"
+            aria-label="Learn more"
+            target="_blank"
+            rel="noreferrer"
+          />
+        </>
+      }
     />
   );
 }
