67754452c7c3d9feac225491bd0501f40c437eee
Wellington Matheus <w.matheus@outlook.com>
2021-06-25 12:35:43 -0300

    feat(Components): create Link component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -19,6 +19,7 @@ import { Input } from './Input';
 import Label from './Label';
 import Layout from './Layout';
 import LineChart from './LineChart';
+import Link from './Link';
 import LiquidityForm from './LiquidityForm';
 import Market from './Market';
 import MarketList from './MarketList';
@@ -76,6 +77,7 @@ export {
   Label,
   Layout,
   LiquidityForm,
+  Link,
   LineChart,
   Market,
   MarketList,
