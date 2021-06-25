667d95cf485edab014764be34f07b79de745f9d5
Wellington Matheus <w.matheus@outlook.com>
2021-06-25 14:12:15 -0300

    feat(CracoConfig): load page and components variables folders

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,11 @@ module.exports = {
         hoistUseStatements: true,
         patterns: [
           path.join(__dirname, './src/styles/abstracts/variables/_colors.scss'),
+          path.join(__dirname, './src/styles/abstracts/variables/pages/*.scss'),
+          path.join(
+            __dirname,
+            './src/styles/abstracts/variables/components/*.scss'
+          ),
           path.join(__dirname, './src/styles/abstracts/variables/*.scss'),
           path.join(__dirname, './src/styles/abstracts/mixins/*.scss'),
           path.join(__dirname, './src/styles/abstracts/functions/*.scss'),
