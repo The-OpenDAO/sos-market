c2bc53bf045e43dffe51f25cfc7069abaffaa524
Wellington Matheus <w.matheus@outlook.com>
2021-06-26 09:05:12 -0300

    feat(Input): create error state styles

--- src/components/Input/OutcomeInput.tsx
+++ src/components/Input/OutcomeInput.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
 
-import Badge, { BadgeColor } from '../Badge';
+import { useField } from 'formik';
 
-type InputProps = {
+import Badge, { BadgeColor } from '../Badge';
+import InputErrorMessage from './InputErrorMessage';
+
+type OutcomeInputProps = {
   name: string;
   badgeColor?: BadgeColor;
 };
 
 const OutcomeInput = React.forwardRef<
   HTMLInputElement,
-  InputProps & React.InputHTMLAttributes<HTMLInputElement>
->(({ name, badgeColor = 'default', ...props }, ref) => (
-  <div className="pm-c-outcome-input__wrapper">
-    <Badge color={badgeColor} />
-    <input ref={ref} className="pm-c-outcome-input" id={name} {...props} />
-  </div>
-));
+  OutcomeInputProps & React.InputHTMLAttributes<HTMLInputElement>
+>(({ name, badgeColor = 'default', ...props }, ref) => {
+  const [field, meta] = useField(name);
+
+  return (
+    <div className="pm-c-outcome-input--default__group">
+      <div
+        className={`pm-c-outcome-input--${
+          meta.error ? 'error' : 'default'
+        }__wrapper`}
+      >
+        <Badge color={badgeColor} />
+        <input
+          ref={ref}
+          className={`pm-c-outcome-input--${meta.error ? 'error' : 'default'}`}
+          id={name}
+          {...field}
+          {...props}
+        />
+      </div>
+      {meta.error ? <InputErrorMessage message={meta.error} /> : null}
+    </div>
+  );
+});
 
 OutcomeInput.displayName = 'OutcomeInput';
 
