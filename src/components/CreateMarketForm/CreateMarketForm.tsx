3d94a2c067305f16b5c4e3023a7e5d03fd226418
Wellington Matheus <w.matheus@outlook.com>
2021-06-26 09:06:09 -0300

    feat(CreateMarketForm): create base Yup validation schema

--- src/components/CreateMarketForm/CreateMarketForm.tsx
+++ src/components/CreateMarketForm/CreateMarketForm.tsx
@@ -1,16 +1,56 @@
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 import CreateMarketFormConfigure from './CreateMarketFormConfigure';
 
-type CreateMarketFormData = {
-  question: string;
+type Outcome = {
+  name: string;
+  probability: number;
 };
 
-function CreateMarketForm() {
-  const initialData: CreateMarketFormData = {
-    question: ''
-  };
+export type CreateMarketFormData = {
+  question: string;
+  firstOutcome: Outcome;
+  secondOutcome: Outcome;
+};
 
+const initialData: CreateMarketFormData = {
+  question: '',
+  firstOutcome: {
+    name: '',
+    probability: 50
+  },
+  secondOutcome: {
+    name: '',
+    probability: 50
+  }
+};
+
+const validationSchema = Yup.object().shape({
+  question: Yup.string().required('Market Question is required!'),
+  firstOutcome: Yup.object().shape({
+    name: Yup.string().required('Outcome name is required!'),
+    probability: Yup.number()
+      .min(0, 'The probability of the Outcome must be greater or equal than 0!')
+      .max(
+        100,
+        'The probability of the Outcome must be less or equal than 100!'
+      )
+      .required('Outcome probability is required!')
+  }),
+  secondOutcome: Yup.object().shape({
+    name: Yup.string().required('Outcome name is required!'),
+    probability: Yup.number()
+      .min(0, 'The probability of the Outcome must be greater or equal than 0!')
+      .max(
+        100,
+        'The probability of the Outcome must be less or equal than 100!'
+      )
+      .required('Outcome probability is required!')
+  })
+});
+
+function CreateMarketForm() {
   async function handleFormSubmit(values: CreateMarketFormData) {
     console.log(values);
   }
@@ -23,6 +63,7 @@ function CreateMarketForm() {
         await handleFormSubmit(values);
         actions.setSubmitting(false);
       }}
+      validationSchema={validationSchema}
     >
       <Form className="pm-c-create-market-form">
         <CreateMarketFormConfigure />
