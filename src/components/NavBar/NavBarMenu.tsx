e347f59b7ed7eb333b23b19669bea545cea0662e
Wellington Matheus <w.matheus@outlook.com>
2021-07-01 09:56:23 -0300

    feat(NavBarMenu): add links to markets and portfolio

--- src/components/NavBar/NavBarMenu.tsx
+++ src/components/NavBar/NavBarMenu.tsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom';
 
+import { useAppSelector } from 'hooks';
+
 import Menu from '../Menu';
 
 function NavBarMenu() {
+  const walletConnected = useAppSelector(state => state.bepro.isLoggedIn);
+
   return (
     <div className="pm-l-navbar__menu">
       <Menu>
+        <Menu.Item key="markets">
+          <Link to="/home">Markets</Link>
+        </Menu.Item>
+        {walletConnected && (
+          <Menu.Item key="portfolio">
+            <Link to="/portfolio">Portfolio</Link>
+          </Menu.Item>
+        )}
         <Menu.Item key="howitworks">
           <Link
             to="//polkamarkets.medium.com/mvp-pre-launch-whitelist-beta-testers-5424ce5fb32"
@@ -18,7 +30,5 @@ function NavBarMenu() {
     </div>
   );
 }
-
-NavBarMenu.displayName = 'NavBarMenu';
 
 export default NavBarMenu;
