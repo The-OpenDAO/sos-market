3ddfd2a2ba262eceda83aabbdd602e7b9e4d01fb
Wellington Matheus <w.matheus@outlook.com>
2021-07-02 10:07:45 -0300

    refactor(NavBarMenu): remove markets and porfolio links

--- src/components/NavBar/NavBarMenu.tsx
+++ src/components/NavBar/NavBarMenu.tsx
@@ -1,23 +1,11 @@
 import { Link } from 'react-router-dom';
 
-import { useAppSelector } from 'hooks';
-
 import Menu from '../Menu';
 
 function NavBarMenu() {
-  const walletConnected = useAppSelector(state => state.bepro.isLoggedIn);
-
   return (
     <div className="pm-l-navbar__menu">
       <Menu>
-        <Menu.Item key="markets">
-          <Link to="/home">Markets</Link>
-        </Menu.Item>
-        {walletConnected && (
-          <Menu.Item key="portfolio">
-            <Link to="/portfolio">Portfolio</Link>
-          </Menu.Item>
-        )}
         <Menu.Item key="howitworks">
           <Link
             to="//polkamarkets.medium.com/mvp-pre-launch-whitelist-beta-testers-5424ce5fb32"
