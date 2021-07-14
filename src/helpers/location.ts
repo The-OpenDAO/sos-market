b86a3257142b6e3dcfde260b668046a38c63fb66
Wellington Matheus <w.matheus@outlook.com>
2021-07-14 12:06:19 -0300

    refactor(LocationHelper): add request error handling

--- src/helpers/location.ts
+++ src/helpers/location.ts
@@ -1,15 +1,30 @@
+import isUndefined from 'lodash/isUndefined';
+import { UserGeolocation } from 'models/user';
 import * as publicIp from 'public-ip';
 import { getUserGeolocation } from 'services/Polkamarkets/user';
 
 async function getUserPublicIp() {
-  return publicIp.v4();
+  try {
+    return publicIp.v4();
+  } catch (err) {
+    return undefined;
+  }
 }
+
+const undefinedCountry: UserGeolocation = { country: '', countryCode: '' };
 
 async function getUserCountry() {
   const userPublicIp = await getUserPublicIp();
-  const { data } = await getUserGeolocation(userPublicIp);
 
-  return data;
+  if (!isUndefined(userPublicIp)) {
+    try {
+      const response = await getUserGeolocation(userPublicIp);
+      return response.data;
+    } catch (err) {
+      return undefinedCountry;
+    }
+  }
+  return undefinedCountry;
 }
 
 export { getUserPublicIp, getUserCountry };
