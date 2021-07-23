5a5ca346dcdbd28bba4b92132b5a153291269026
Wellington Matheus <w.matheus@outlook.com>
2021-07-23 06:06:27 -0300

    refactor(Ribbon): create color mixin and variables

--- src/components/Ribbon/Ribbon.tsx
+++ src/components/Ribbon/Ribbon.tsx
@@ -14,7 +14,12 @@ function Ribbon({ color = 'pink', icon, text }: RibbonProps) {
   return (
     <div className={`pm-c-ribbon--${color}`}>
       {icon || null}
-      <Text as="span" scale="tiny-uppercase" fontWeight="semibold">
+      <Text
+        as="span"
+        scale="tiny-uppercase"
+        fontWeight="semibold"
+        className="pm-c-ribbon__text"
+      >
         {text}
       </Text>
     </div>
