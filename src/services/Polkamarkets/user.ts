98aa35969037962efe43a491fdbfc8bf42557a56
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-08-03 17:36:42 +0100

    fix(GeoLocation): upgrade ipApi to pro

--- src/services/Polkamarkets/user.ts
+++ src/services/Polkamarkets/user.ts
@@ -4,7 +4,8 @@ import { UserGeolocation } from 'models/user';
 import api from './api';
 
 async function getUserGeolocation(ipAddress: string) {
-  const url = `http://ip-api.com/json/${ipAddress}`;
+  const { REACT_APP_IP_API_KEY } = process.env;
+  const url = `https://pro.ip-api.com/json?key=${REACT_APP_IP_API_KEY}`;
   return api.get<UserGeolocation>(url, {
     params: {
       fields: 'country,countryCode'
