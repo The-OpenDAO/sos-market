036df8ba57af9f14f0937faab60364b86efdffa2
Wellington Matheus <w.matheus@outlook.com>
2021-08-11 16:14:22 -0300

    feat(Ribbon): create warning variant

--- src/components/Ribbon/Ribbon.tsx
+++ src/components/Ribbon/Ribbon.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 import Text from '../Text';
 
-export type RibbonColor = 'pink' | 'blue' | 'purple';
+export type RibbonColor = 'pink' | 'blue' | 'purple' | 'warning';
 
 type RibbonProps = {
   color?: RibbonColor;
