1a40bfb89447faa5a6f4ffc56d0298d6ddf17d4f
Wellington Matheus <w.matheus@outlook.com>
2021-08-12 08:23:20 -0300

    feat(ReportFormActions): create approve POLK use action

--- src/components/ReportForm/ReportFormActions.tsx
+++ src/components/ReportForm/ReportFormActions.tsx
@@ -1,29 +1,75 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { useField, useFormikContext } from 'formik';
 import isUndefined from 'lodash/isUndefined';
+import { login } from 'redux/ducks/bepro';
 import { closeReportForm } from 'redux/ducks/ui';
 import { BeproService, PolkamarketsApiService } from 'services';
 
+import { QuestionIcon } from 'assets/icons';
+
 import { useAppDispatch, useAppSelector } from 'hooks';
+import useToastNotification from 'hooks/useToastNotification';
 
 import { Button } from '../Button';
+import Toast from '../Toast';
+import ToastNotification from '../ToastNotification';
+import Tooltip from '../Tooltip';
 
 function ReportFormActions() {
+  // Helpers
   const location = useLocation();
   const dispatch = useAppDispatch();
+  const { show, close } = useToastNotification();
 
-  const marketSlug = useAppSelector(state => state.market.market.slug);
-  const { questionId } = useAppSelector(state => state.market.market);
-  // bond must be doubled in every tx
-  const marketBond = useAppSelector(state => state.market.market.question.bond);
-  const minimumBond = marketBond * 2;
-
+  // Form state
   const [outcome] = useField('outcome');
   const [bond] = useField('bond');
   const { isSubmitting } = useFormikContext();
 
+  // Loading state
+  const [isApprovingPolk, setIsApprovingPolk] = useState(false);
+  const [approvePolkTransactionSuccess, setApprovePolkTransactionSuccess] =
+    useState(false);
+  const [
+    approvePolkTransactionSuccessHash,
+    setApprovePolkTransactionSuccessHash
+  ] = useState(undefined);
+
+  // Selectors
+  const marketSlug = useAppSelector(state => state.market.market.slug);
+  const isPolkApproved = useAppSelector(state => state.bepro.polkApproved);
+  const { questionId } = useAppSelector(state => state.market.market);
+  // bond must be doubled in every tx
+  const marketBond = useAppSelector(state => state.market.market.question.bond);
+
+  // Derivated state
+  const minimumBond = marketBond * 2;
   const isMarketPage = location.pathname === `/markets/${marketSlug}`;
+
+  async function handleApprovePolk() {
+    const beproService = new BeproService();
+
+    setIsApprovingPolk(true);
+
+    try {
+      const response = await beproService.approveRealitioERC20();
+      const { status, transactionHash } = response;
+
+      if (status && transactionHash) {
+        setApprovePolkTransactionSuccess(status);
+        setApprovePolkTransactionSuccessHash(transactionHash);
+        show('approvePolk');
+      }
+
+      await login(dispatch);
+      setIsApprovingPolk(false);
+    } catch (error) {
+      setIsApprovingPolk(false);
+      console.error(error);
+    }
+  }
 
   function handleCancel() {
     dispatch(closeReportForm());
@@ -48,27 +94,80 @@ function ReportFormActions() {
 
   return (
     <div className="pm-c-report-form-details__actions">
-      {!isMarketPage ? (
-        <Button variant="subtle" color="default" onClick={handleCancel}>
-          Cancel
-        </Button>
-      ) : null}
-      <Button
-        type="submit"
-        color="primary"
-        fullwidth
-        onClick={handleBond}
-        disabled={
-          !outcome.value ||
-          !bond.value ||
-          bond.value === 0 ||
-          isUndefined(minimumBond) ||
-          bond.value < minimumBond
-        }
-        loading={isSubmitting}
-      >
-        Bond
-      </Button>
+      <div className="pm-c-report-form-details__actions-group--column">
+        {!isPolkApproved ? (
+          <Button
+            color="primary"
+            size="sm"
+            fullwidth
+            style={{
+              justifyContent: isApprovingPolk ? 'center' : 'space-between'
+            }}
+            onClick={handleApprovePolk}
+            loading={isApprovingPolk}
+            disabled={isApprovingPolk}
+          >
+            Allow Polkamarkets to use your POLK
+            <Tooltip text="Example">
+              <QuestionIcon
+                style={{ width: '1.4rem', height: '1.4rem', opacity: 0.35 }}
+              />
+            </Tooltip>
+          </Button>
+        ) : null}
+        {approvePolkTransactionSuccess && approvePolkTransactionSuccessHash ? (
+          <ToastNotification id="approvePolk" duration={10000}>
+            <Toast
+              variant="success"
+              title="Success"
+              description="Your transaction is completed!"
+            >
+              <Toast.Actions>
+                <a
+                  target="_blank"
+                  href={`https://kovan.etherscan.io/tx/${approvePolkTransactionSuccessHash}`}
+                  rel="noreferrer"
+                >
+                  <Button size="sm" color="success">
+                    View on Explorer
+                  </Button>
+                </a>
+                <Button
+                  size="sm"
+                  variant="ghost"
+                  onClick={() => close('approvePolk')}
+                >
+                  Dismiss
+                </Button>
+              </Toast.Actions>
+            </Toast>
+          </ToastNotification>
+        ) : null}
+        <div className="pm-c-report-form-details__actions-group--row">
+          {!isMarketPage ? (
+            <Button variant="subtle" color="default" onClick={handleCancel}>
+              Cancel
+            </Button>
+          ) : null}
+          <Button
+            type="submit"
+            color="primary"
+            fullwidth
+            onClick={handleBond}
+            disabled={
+              !isPolkApproved ||
+              !outcome.value ||
+              !bond.value ||
+              bond.value === 0 ||
+              isUndefined(minimumBond) ||
+              bond.value < minimumBond
+            }
+            loading={isSubmitting}
+          >
+            Bond
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
