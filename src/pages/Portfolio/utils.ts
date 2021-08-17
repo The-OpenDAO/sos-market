65524ec69e57f43b126b0fd17bd56e5ae1b92220
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-08-17 18:27:18 +0300

    fix(Portfolio): remove comment

--- src/pages/Portfolio/utils.ts
+++ src/pages/Portfolio/utils.ts
@@ -298,9 +298,9 @@ function formatReportPositions(bonds: Object, markets: Market[]) {
         bonds[market.questionId]?.withdrawn ||
         (market.question.isClaimed && payout === 0)
       ) {
-        // user still has report tokens to claim
         result = { type: 'claimed' };
       } else if (market.question.isFinalized) {
+        // user still has report tokens to claim
         result = { type: 'awaiting_claim' };
       }
 
