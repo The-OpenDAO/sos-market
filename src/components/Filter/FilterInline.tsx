141346b515071a8b64b8df31afb17e33eca325fa
Wellington Matheus <w.matheus@outlook.com>
2021-08-30 09:06:18 -0300

    feat(FilterInline): add help text prop

--- src/components/Filter/FilterInline.tsx
+++ src/components/Filter/FilterInline.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable no-unused-vars */
 import { ChangeEvent } from 'react';
 
+import { InfoIcon } from 'assets/icons';
+
 import Checkbox from '../Checkbox';
+import Tooltip from '../Tooltip';
 
 type FilterInlineProps = {
   label: string;
+  helpText: string;
   onChange: (checked: boolean) => void;
 };
 
-function FilterInline({ label, onChange }: FilterInlineProps) {
+function FilterInline({ label, helpText, onChange }: FilterInlineProps) {
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const { checked } = event.currentTarget;
     onChange(checked);
@@ -17,6 +21,9 @@ function FilterInline({ label, onChange }: FilterInlineProps) {
   return (
     <div className="pm-c-filter-inline">
       <Checkbox label={label} onChange={event => handleChange(event)} />
+      <Tooltip text={helpText} position="top">
+        <InfoIcon />
+      </Tooltip>
     </div>
   );
 }
