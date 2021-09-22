87b8409e3e2474b9cf48ddb5e8c5dac80862dc47
Wellington Matheus <w.matheus@outlook.com>
2021-09-22 10:55:03 -0300

    feat(useNetwork): add explorerURL

--- src/hooks/useNetwork.tsx
+++ src/hooks/useNetwork.tsx
@@ -16,34 +16,75 @@ type Network = {
   name: string;
   key: string;
   currency: string;
+  explorerURL: string;
 };
 
-const ethereumNetworks = {
-  '0x1': { id: '1', name: 'Ethereum Mainnet', key: 'mainnet', currency: 'ETH' },
-  '0x3': { id: '3', name: 'Ropsten Testnet', key: 'ropsten', currency: 'ETH' },
-  '0x4': { id: '4', name: 'Rinkeby Testnet', key: 'rinkeby', currency: 'ETH' },
-  '0x5': { id: '5', name: 'Goerli Testnet', key: 'goerli', currency: 'ETH' },
-  '0x2a': { id: '42', name: 'Kovan Testnet', key: 'kovan', currency: 'ETH' },
+const networks: {
+  [key: string]: Network;
+} = {
+  '0x1': {
+    id: '1',
+    name: 'Ethereum Mainnet',
+    key: 'mainnet',
+    currency: 'ETH',
+    explorerURL: 'https://etherscan.io'
+  },
+  '0x3': {
+    id: '3',
+    name: 'Ropsten Testnet',
+    key: 'ropsten',
+    currency: 'ETH',
+    explorerURL: 'https://ropsten.etherscan.io'
+  },
+  '0x4': {
+    id: '4',
+    name: 'Rinkeby Testnet',
+    key: 'rinkeby',
+    currency: 'ETH',
+    explorerURL: 'https://rinkeby.etherscan.io'
+  },
+  '0x5': {
+    id: '5',
+    name: 'Goerli Testnet',
+    key: 'goerli',
+    currency: 'ETH',
+    explorerURL: 'https://goerli.etherscan.io'
+  },
+  '0x2a': {
+    id: '42',
+    name: 'Kovan Testnet',
+    key: 'kovan',
+    currency: 'ETH',
+    explorerURL: 'https://kovan.etherscan.io'
+  },
   '0x501': {
     id: '1281',
     name: 'Moonbase Local',
     key: 'moonbase-local',
-    currency: 'DEV'
+    currency: 'DEV',
+    explorerURL: 'https://etherscan.io'
   },
   '0x507': {
     id: '1287',
     name: 'Moonbase Alpha',
     key: 'moonbase-alpha',
-    currency: 'DEV'
+    currency: 'DEV',
+    explorerURL: 'https://etherscan.io'
   },
-  '0x539': { id: '1337', name: 'Local Testnet', key: 'local', currency: 'ETH' }
+  '0x539': {
+    id: '1337',
+    name: 'Local Testnet',
+    key: 'local',
+    currency: 'ETH',
+    explorerURL: 'https://etherscan.io'
+  }
 };
 
 export const defaultNetwork = (): Network => {
   const networkId = Number(process.env.REACT_APP_NETWORK_ID) || 42;
   const networkIdHex = `0x${networkId.toString(16)}`;
 
-  return ethereumNetworks[networkIdHex];
+  return networks[networkIdHex];
 };
 
 function useNetwork() {
@@ -55,7 +96,7 @@ function useNetwork() {
     window.ethereum?.on('accountsChanged', async () => {
       const chainId = await window.ethereum?.request({ method: 'eth_chainId' });
 
-      if (defaultNetwork() === ethereumNetworks[chainId]) {
+      if (defaultNetwork() === networks[chainId]) {
         login(store.dispatch);
         fetchAditionalData(store.dispatch);
       }
@@ -64,9 +105,9 @@ function useNetwork() {
 
   function checkNetworkObserver() {
     window.ethereum?.on('chainChanged', chainId => {
-      setNetwork(ethereumNetworks[chainId]);
+      setNetwork(networks[chainId]);
 
-      if (defaultNetwork() === ethereumNetworks[chainId]) {
+      if (defaultNetwork() === networks[chainId]) {
         login(store.dispatch);
         fetchAditionalData(store.dispatch);
       }
@@ -76,7 +117,7 @@ function useNetwork() {
   async function getChainId() {
     const chainId = await window.ethereum?.request({ method: 'eth_chainId' });
 
-    setNetwork(ethereumNetworks[chainId]);
+    setNetwork(networks[chainId]);
   }
 
   async function checkNetwork() {
