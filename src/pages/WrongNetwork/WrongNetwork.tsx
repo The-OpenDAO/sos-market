9dc0fd4580d07ac7e531cc2ed20f5177692cb69b
Wellington Matheus <w.matheus@outlook.com>
2021-09-23 08:36:35 -0300

    feat(WrongNetwork): add switch chain method

--- src/pages/WrongNetwork/WrongNetwork.tsx
+++ src/pages/WrongNetwork/WrongNetwork.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 
+import findKey from 'lodash/findKey';
+
 import { Button, ModalNotification, Text } from 'components';
 
 import { getDefaultNetwork } from 'hooks/useNetwork';
+import NETWORKS from 'hooks/useNetwork/networks';
 
 function WrongNetwork() {
   const [isAddingNetwork, setIsAddingNetwork] = useState(false);
@@ -12,27 +15,41 @@ function WrongNetwork() {
     setIsAddingNetwork(true);
 
     try {
-      await window.ethereum.request({
-        method: 'wallet_addEthereumChain',
-        params: [
-          {
-            chainId: '0x505',
-            chainName: 'Moonriver',
-            nativeCurrency: {
-              name: 'MOVR',
-              symbol: 'MOVR',
-              decimals: 18
-            },
-            rpcUrls: ['https://rpc.moonriver.moonbeam.network'],
-            blockExplorerUrls: [
-              'https://blockscout.moonriver.moonbeam.network/'
-            ]
-          }
-        ]
-      });
+      try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [
+            {
+              chainId: findKey(NETWORKS, defaultNetwork)
+            }
+          ]
+        });
 
-      setIsAddingNetwork(false);
-    } catch (error) {
+        setIsAddingNetwork(false);
+      } catch (error: any) {
+        if (error.code === 4902) {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x505',
+                chainName: 'Moonriver',
+                nativeCurrency: {
+                  name: 'MOVR',
+                  symbol: 'MOVR',
+                  decimals: 18
+                },
+                rpcUrls: ['https://rpc.moonriver.moonbeam.network'],
+                blockExplorerUrls: [
+                  'https://blockscout.moonriver.moonbeam.network/'
+                ]
+              }
+            ]
+          });
+        }
+        setIsAddingNetwork(false);
+      }
+    } catch (error: any) {
       setIsAddingNetwork(false);
     }
   }
