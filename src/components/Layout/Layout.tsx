eedb2e88cb3848efeb531b4b09ef4c6a86d21795
Wellington Matheus <w.matheus@outlook.com>
2021-09-24 11:09:14 -0300

    feat(Modal): create overlay and wrapper

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from 'react';
 
 import useAlertNotification from 'hooks/useAlertNotification';
 
+import BetaWarning from '../BetaWarning';
 import Footer from '../Footer';
+import Modal from '../Modal';
 import NavBar from '../NavBar';
 import RightSidebar from '../RightSidebar';
 import ScrollableArea from '../ScrollableArea';
@@ -18,36 +20,41 @@ function Layout({ children }: LayoutProps) {
   const hasAlertNotification = alertList.size > 0;
 
   return (
-    <div className="pm-l-layout">
-      <header className="pm-l-layout__header sticky">
-        <div id="alert-notification-portal" />
-        <NavBar />
-      </header>
-      <nav className="pm-l-layout__nav">
-        <Sidebar />
-      </nav>
-      <ScrollableArea
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: hasAlertNotification
-            ? 'calc(100vh - 11rem)'
-            : 'calc(100vh - 7.3rem)'
-        }}
-      >
-        <main className="pm-l-layout__main">{children}</main>
-        <footer className="pm-l-layout__footer">
-          <Footer />
-        </footer>
-      </ScrollableArea>
-      <ScrollableArea>
-        <aside className="pm-l-layout__aside">
-          <RightSidebar hasAlertNotification={hasAlertNotification} />
-          <div id="toast-notification-portal" />
-        </aside>
-      </ScrollableArea>
-    </div>
+    <>
+      <Modal>
+        <BetaWarning />
+      </Modal>
+      <div className="pm-l-layout">
+        <header className="pm-l-layout__header sticky">
+          <div id="alert-notification-portal" />
+          <NavBar />
+        </header>
+        <nav className="pm-l-layout__nav">
+          <Sidebar />
+        </nav>
+        <ScrollableArea
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            height: hasAlertNotification
+              ? 'calc(100vh - 11rem)'
+              : 'calc(100vh - 7.3rem)'
+          }}
+        >
+          <main className="pm-l-layout__main">{children}</main>
+          <footer className="pm-l-layout__footer">
+            <Footer />
+          </footer>
+        </ScrollableArea>
+        <ScrollableArea>
+          <aside className="pm-l-layout__aside">
+            <RightSidebar hasAlertNotification={hasAlertNotification} />
+            <div id="toast-notification-portal" />
+          </aside>
+        </ScrollableArea>
+      </div>
+    </>
   );
 }
 
