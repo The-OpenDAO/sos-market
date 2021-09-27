f813caead75a49ec49fe29dd5f615fdd39961215
Wellington Matheus <w.matheus@outlook.com>
2021-09-27 06:22:41 -0300

    fix(NavBarActions): use network ticker

--- src/components/NavBar/NavBarActions.tsx
+++ src/components/NavBar/NavBarActions.tsx
@@ -44,7 +44,7 @@ function NavBarActions() {
         variant="warning"
         description={
           <>
-            {`Welcome to Polkamarkets! You’re on ${network.name} and placing predictions with ${network.currency}. Your `}
+            {`Welcome to Polkamarkets! You’re on ${network.name} and placing predictions with ${network.currency.ticker}. Your `}
             <Link
               title="feedback"
               target="_blank"
