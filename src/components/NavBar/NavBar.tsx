9b13d46aaecf9adae8a4fb3c49db46825d905c99
Wellington Matheus <w.matheus@outlook.com>
2021-10-01 09:34:55 -0300

    feat(NavBar): add risk disclosure doc url

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 
 import { setFilter } from 'redux/ducks/markets';
 
-import { PolkamarketsBetaIcon } from 'assets/icons';
+import { PolkamarketsIcon, BetaTagIcon } from 'assets/icons';
 
 import NavBarActions from './NavBarActions';
 import NavBarDropdownMenu from './NavBarDropdownMenu';
 import NavBarMenu from './NavBarMenu';
 import NavBarSearch from './NavBarSearch';
+
+const RISK_DISCLOSURE_DOC_URL =
+  'https://docs.google.com/document/d/1TR8HYTBOhZeZOb0E5uAo8lbK4v0Oxv3JnQD_AdYENBY/edit';
 
 function NavBar() {
   const dispatch = useDispatch();
@@ -20,11 +23,14 @@ function NavBar() {
 
   return (
     <div className="pm-l-navbar">
-      <Link to="/home" aria-label="Home" onClick={handleNavigation}>
-        <figure className="pm-l-navbar__icon">
-          <PolkamarketsBetaIcon />
-        </figure>
-      </Link>
+      <figure className="pm-l-navbar__icon">
+        <Link to="/home" aria-label="Home" onClick={handleNavigation}>
+          <PolkamarketsIcon />
+        </Link>
+        <a href={RISK_DISCLOSURE_DOC_URL} target="_blank" rel="noreferrer">
+          <BetaTagIcon />
+        </a>
+      </figure>
       <NavBarSearch />
       <NavBarMenu />
       <NavBarActions />
