ef2afd77a3ac13a448b6d96479460a6553aea0b5
Wellington Matheus <w.matheus@outlook.com>
2021-10-15 16:20:15 -0300

    feat(App): add SEO meta

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import DayjsUtils from '@date-io/dayjs';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import Routes from 'routes';
 
+import { SEO } from 'components';
+
 import FavoriteMarketsProvider from 'contexts/favoriteMarkets';
 
 import { useLocalStorage, useTheme } from 'hooks';
@@ -18,6 +20,10 @@ const App = () => {
 
   return (
     <div className={`theme--${localStorageTheme || theme}`}>
+      <SEO
+        title="Polkamarkets - Gamified Prediction Markets"
+        description="Polkamarkets is a DeFi-Powered Prediction Market built for cross-chain information exchange, based on Polkadot."
+      />
       <MuiPickersUtilsProvider utils={DayjsUtils}>
         <FavoriteMarketsProvider>
           <Routes />
