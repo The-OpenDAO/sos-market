fb7d130abd7b0a2f9396a4727c5a600037836bee
RowenXZ <rowen.pizzap.io>
2022-01-21 20:01:15 +0800

    hide vif / 2

--- src/components/CreateMarketForm/CreateMarketFormFund.tsx
+++ src/components/CreateMarketForm/CreateMarketFormFund.tsx
@@ -141,6 +141,26 @@ function CreateMarketFormFund() {
             {`${roundNumber((fee / 2) * 100, 3)} %`}
           </Text>
         </div>
+
+        <div className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee">
+          <Text
+            as="span"
+            scale="caption"
+            fontWeight="semibold"
+            className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee__title"
+          >
+            veSOS pool
+          </Text>
+
+          <Text
+            as="span"
+            scale="caption"
+            fontWeight="semibold"
+            className="pm-c-create-market-form__card-liquidity-details__earn-trading-fee__amount"
+          >
+            {`${roundNumber((fee / 2) * 100, 3)} %`}
+          </Text>
+        </div>
       </div>
     </div>
   );
