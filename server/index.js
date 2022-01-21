3471224cd0d9ffdcd73e4cc3ab08c1021e115020
Tony Ke <thagki9@outlook.com>
2022-01-21 10:59:01 +0800

    Revert redirect change

--- server/index.js
+++ server/index.js
@@ -34,11 +34,11 @@ const defaultMetadataTemplate = (request, htmlData) => {
   });
 };
 
-app.use((req, res, next) => {
-  req.secure
-    ? next()
-    : res.redirect('https://' + req.headers.host + req.url);
-})
+// app.use((req, res, next) => {
+//   req.secure
+//     ? next()
+//     : res.redirect('https://' + req.headers.host + req.url);
+// })
 
 app.get('/', (request, response) => {
   fs.readFile(indexPath, 'utf8', async (error, htmlData) => {
